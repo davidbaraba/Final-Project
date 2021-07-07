bbf2f25c20c533bb61408c72c55ad407b78d3182
davidbaraba <62241374+davidbaraba@users.noreply.github.com>
2021-07-07 09:13:33 +0400

    create servises section and add responsive styles.

--- script.js
+++ script.js
@@ -21,3 +21,23 @@ function createMainSlider(){
     }
 }
 createMainSlider();
+
+function createModal(){
+    let test = document.querySelectorAll('#test');
+
+    for (let i = 0; i < test.length; i++) {
+        test[i].addEventListener('mouseover', function(){
+            this.style.backgroundColor = '#15263E';
+            this.childNodes[1].style.display = 'none';
+            this.childNodes[3].style.display = 'none';
+            this.childNodes[5].style.display = 'block';
+        });
+        test[i].addEventListener('mouseout', function(){
+            this.style.backgroundColor = '#FAFAFA';
+            this.childNodes[1].style.display = 'flex';
+            this.childNodes[3].style.display = 'flex';
+            this.childNodes[5].style.display = 'none';
+        });
+    }
+}
+createModal();
