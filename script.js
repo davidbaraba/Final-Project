59d0a5679f282aea7e3f866649e5432901e7d930
davidbaraba <62241374+davidbaraba@users.noreply.github.com>
2021-07-18 19:45:44 +0400

    edit project details.

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function createMainSlider(){
     let sliderImageArray = [
         'images/slider1.jpg',
         'images/slider2.jpg',
-        'images/slider3.jpg'
+        'images/slider3.jpg',
     ]
     
-    let currentSlideIndex = 1;
+    let currentSlideIndex = 0;
     
     let slider = document.getElementById('sectionSlider');
     
@@ -26,16 +26,16 @@ function createMySkillsAnimation(){
 
     window.addEventListener('scroll', function(){
         let scrollTop = document.querySelector('html').scrollTop;
-        let elem = document.querySelectorAll('.skill-div-line');
-        let elem1 = document.querySelectorAll('.skill-div-line-modal');
+        let line = document.querySelectorAll('.skill-div-line');
+        let modalLine = document.querySelectorAll('.skill-div-line-modal');
 
         if(scrollTop < 1770 && scrollTop > 900){
-            for (let i = 0; i < elem.length; i++) {
-                elem[i].classList.add('active')                
+            for (let i = 0; i < line.length; i++) {
+                line[i].classList.add('active')                
             }
 
-            for (let i = 0; i < elem1.length; i++) {
-                elem1[i].classList.add('active');        
+            for (let i = 0; i < modalLine.length; i++) {
+                modalLine[i].classList.add('active');        
             }
 
             mySkillsHtml.innerText = '( 80% )';
@@ -50,12 +50,12 @@ function createMySkillsAnimation(){
             mySkillsJavascript.innerText = '( 50% )';
             mySkillsJavascript.style.color = '#E93656';
         }else{
-            for (let i = 0; i < elem.length; i++) {
-                elem[i].classList.remove('active')                
+            for (let i = 0; i < line.length; i++) {
+                line[i].classList.remove('active')                
             }
 
-            for (let i = 0; i < elem1.length; i++) {
-                elem1[i].classList.remove('active');        
+            for (let i = 0; i < modalLine.length; i++) {
+                modalLine[i].classList.remove('active');        
             }
 
             mySkillsHtml.innerText = '';
@@ -90,19 +90,19 @@ createServicesModal();
 function createRecommendationsSlider(){
     let usersRecommendations = [
         {
-            text: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est dolor sit amet, consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore.",
+            text: "Working with David was better than expected and we had really high expectations. He is an incredibly talented developer but what really makes him stand out is his work ethic and...",
             image: 'images/portrait1.png',
             profession: 'Full-stack Developer',
             author: 'Nick'
         },
         {
-            text: "I want you to know how much we enjoy serving your hardware needs and consider you a special customer. Of course we appreciate your orders, but we also appreciate the positive lift we get from your visits. As a token of our appreciation, I am enclosing a coupon worth 10% off your next purchase. Come visit us soon.",
+            text: "David has done a fantastic job overall. Not only the site is to design, the code is very clean and slick. Love the way he achieved the translations portion of the...",
             image: 'images/portrait2.png',
             profession: 'Front-end Developer',
             author: 'Ana'
         },
         {
-            text: "Thank you notes are appropriate after many different occasions. Anyone who throws a party, gives a gift, or takes time to do something for you deserves to be appreciated. ",
+            text: "If you need, Responsive, Cross Browser Compatible and latest designs, David Barabadze is the Man for you. Out of a score of 100 i'd rate him a 110 .",
             image: 'images/portrait3.jpg',
             profession: 'Back-end Developer',
             author: 'John'
@@ -168,11 +168,17 @@ createRecommendationsSlider();
 function createProjectsFilter(){
     const projectsList = document.querySelectorAll('.projects-li');
     const projects = document.querySelectorAll('.project');
+    const  projectsListLine = document.querySelectorAll('.projects-li>span');
 
     for (let i = 0; i < projectsList.length; i++) {
 
         projectsList[i].addEventListener('click', (e)=>{
             e.preventDefault();
+
+            for (let j = 0; j < projectsListLine.length; j++) {
+                projectsListLine[j].classList.remove('active');
+            }
+            e.target.classList.add('active');
 
             for (let x = 0; x < projectsList.length; x++) {
                 projectsList[x].classList.remove('active');
@@ -183,10 +189,10 @@ function createProjectsFilter(){
             
             projects.forEach((project)=>{
                 if(filter == "all"){
-                    project.style.display = "block";
+                    project.style.display = "flex";
                 }else{
                     if(project.classList.contains(filter)){
-                        project.style.display = "block";
+                        project.style.display = "flex";
                     }else{
                         project.style.display = "none";
                     }
@@ -198,12 +204,11 @@ function createProjectsFilter(){
 
     for (let p = 0; p < projects.length; p++) {
         projects[p].addEventListener('mouseover', function(){
-            this.children[0].style.display = 'none';
-            this.children[1].style.display = 'flex';
+            this.children[0].children[0].style.display = 'flex';
+            this.children[0].children[0].style.backgroundColor = 'rgba(0, 0, 0, 0.7)'
         });
         projects[p].addEventListener('mouseout', function(){
-            this.children[0].style.display = 'flex';
-            this.children[1].style.display = 'none';
+            this.children[0].children[0].style.display = 'none';
         });
     }
 }
@@ -233,7 +238,7 @@ function sendContactInfo(){
         let userText = document.getElementById('userText').value;
     
         sendInfo(userName, userEmail, userUrl, userText);
-    
+
         document.getElementById('userName').value = '';
         document.getElementById('userEmail').value = '';
         document.getElementById('userUrl').value = '';
